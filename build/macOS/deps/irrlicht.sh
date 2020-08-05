32d4ab27a036eca986f9e954e904c9d3caebbe9c
Maksim <MoNTE48@mail.ua>
2020-08-05 19:22:56 +0200

    Update irrlicht build script

--- build/macOS/deps/irrlicht.sh
+++ build/macOS/deps/irrlicht.sh
@@ -1,22 +1,26 @@
 #!/bin/bash -e
 
-. sdk.sh
+#. sdk.sh
 
-[ ! -d irrlicht-src ] && \
-	git clone -b ogl-es --depth 1 https://github.com/MoNTE48/Irrlicht irrlicht-src
+#[ ! -d irrlicht-src ] && \
+#	git clone -b ogl-es --depth 1 https://github.com/MoNTE48/Irrlicht irrlicht-src
 
-cd irrlicht-src/source/Irrlicht
+cd irrlicht-src/source/Irrlicht/MacOSX
 xcodebuild build \
-	-project Irrlicht.xcodeproj \
+	-project MacOSX.xcodeproj \
 	-configuration Release \
-	-scheme Irrlicht_iOS \
-	-destination generic/platform=iOS
-cd ../..
+	-scheme libIrrlicht.a
+BUILD_FOLDER=$(xcodebuild -project MacOSX.xcodeproj -scheme \
+        libIrrlicht.a -showBuildSettings |\
+        grep TARGET_BUILD_DIR | sed -n -e 's/^.*TARGET_BUILD_DIR = //p')
+cd ../../..
 
-[ -d ../irrlicht ] && rm -r ../irrlicht
+#[ -d ../irrlicht ] && rm -r ../irrlicht
 mkdir -p ../irrlicht
-cp lib/iOS/libIrrlicht.a ../irrlicht/
-cp -r include ../irrlicht/include
-cp -r media/Shaders ../irrlicht/shaders
+#cp lib/iOS/libIrrlicht.a ../irrlicht/
+cp "${BUILD_FOLDER}/libIrrlicht.a" ../irrlicht
+#Avoid rebuild everytime
+#cp -r include ../irrlicht/include
+#cp -r media/Shaders ../irrlicht/shaders
 
 echo "Irrlicht build successful"
